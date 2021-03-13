d77a263a2491b0747bef17f7c4908147ae1d150a
webtoor <fityanalimunshi@gmail.com>
2021-03-14 02:11:49 +0700

    create func add sensorId & sensorValue

--- src/VuenicWifiHTTP.cpp
+++ src/VuenicWifiHTTP.cpp
@@ -4,6 +4,46 @@ VuenicWifiHTTP::VuenicWifiHTTP(String apiKey) {
     _apiKey = apiKey;
 }
 
+// String Value
+void VuenicESP8266HTTP::add(String key, String value) {
+    DynamicJsonDocument doc(256);
+    deserializeJson(doc, jsonString);    
+    doc[key] = value;
+    String newInsert;
+    serializeJson(doc, newInsert);
+    jsonString = newInsert;
+}
+
+// Integer Value
+void VuenicESP8266HTTP::add(String key, int value) {
+    DynamicJsonDocument doc(256);
+    deserializeJson(doc, jsonString);    
+    doc[key] = value;
+    String newInsert;
+    serializeJson(doc, newInsert);
+    jsonString = newInsert;
+}
+
+// Float Value
+void VuenicESP8266HTTP::add(String key, float value) {
+    DynamicJsonDocument doc(256);
+    deserializeJson(doc, jsonString);    
+    doc[key] = value;
+    String newInsert;
+    serializeJson(doc, newInsert);
+    jsonString = newInsert;
+}
+
+// Double Value
+void VuenicESP8266HTTP::add(String key, double value) {
+    DynamicJsonDocument doc(256);
+    deserializeJson(doc, jsonString);    
+    doc[key] = value;
+    String newInsert;
+    serializeJson(doc, newInsert);
+    jsonString = newInsert;
+}
+
 bool VuenicWifiHTTP::wifiConnection(String wifiSSID, String wifiPassword) {
     char wifiSSIDChar[wifiSSID.length()+1];
     char wifiPasswordChar[wifiPassword.length()+1];
--- src/VuenicWifiHTTP.h
+++ src/VuenicWifiHTTP.h
@@ -1,16 +1,23 @@
 #ifndef VuenicWifiHTTP_H
 #define VuenicWifiHTTP_H
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <WiFiEsp.h>
 
 class VuenicWifiHTTP
 {
     public:
       VuenicWifiHTTP(String apiKey);
+      void begin();
+      void add(String key, String value);
+      void add(String key, int value);
+      void add(String key, float value);
+      void add(String key, double value);
       bool wifiConnection(String wifiSSID, String wifiPassword);
       
     private:
       String _apiKey;
+      String jsonString = "{}";
       char* _wifiSSID;
       char* _wifiPass;
       String ipToString(IPAddress ip);
